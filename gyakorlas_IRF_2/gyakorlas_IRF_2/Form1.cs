fb3489204d416144c94a744eb5aa1be1d0c33228
Noémi Bakó <noemi.bako@stud.uni-corvinus.hu>
2021-10-10 11:59:18 +0200

    Add new user

--- gyakorlas_IRF_2/gyakorlas_IRF_2/Form1.cs
+++ gyakorlas_IRF_2/gyakorlas_IRF_2/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyakorlas_IRF_2.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,25 @@ namespace gyakorlas_IRF_2
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+
+
+            };
+            users.Add(u);
         }
     }
 }
